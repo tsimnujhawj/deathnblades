4b9023c2d931efe31132c8b4d1e6fbb5c756a47e
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-06-03 01:56:33 -0500

    building layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+// Set character objects
+
+var yasuo = {
+    hp: 200,
+    atk: function() {
+        var atkDmg = 10 * eachAtk;
+    },
+    def: function() {
+        var resistance = 10;
+    },
+}
+
+// Set global variables
+var eachAtk = 0;
